cf959f933fdfbbb23dd1e3695bef297732e14d25
David Hess <ddtmhess@gmail.com>
2024-06-18 23:09:49 +0000

    updated file

--- src/main/java/com/sandbox/company/entity/AssignmentPK.java
+++ src/main/java/com/sandbox/company/entity/AssignmentPK.java
@@ -2,29 +2,14 @@ package com.sandbox.company.entity;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Embeddable;
 import lombok.*;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Embeddable
 public class AssignmentPK implements Serializable {
 
     private Long employee;
-    protected Long project;
+    private Long project;
 
-    // public boolean equals(Object object) {
-    // if (object instanceof PhonePK) {
-    // PhonePK pk = (PhonePK)object;
-    // return type.equals(pk.type) && owner == pk.owner;
-    // } else {
-    // return false;
-    // }
-    // }
-
-    // public int hashCode() {
-    // return (int)(type.hashCode() + owner);
-    // }
 }
