d2d52a5d8a853829cad906c354c219fc62053394
David Hess <ddtmhess@gmail.com>
2024-06-18 20:19:49 +0000

    troubleshooting

--- src/main/java/com/sandbox/company/entity/Assignment.java
+++ src/main/java/com/sandbox/company/entity/Assignment.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
@@ -21,22 +22,19 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-// @IdClass(AssignmentPK.class)
+@IdClass(AssignmentPK.class)
 @Table(name = "assignment", uniqueConstraints = {
         @UniqueConstraint(columnNames = { "employee_id", "project_id" })
 })
 public class Assignment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "assignment_id")
-    private Long id;
-
     @ManyToOne(optional = false)
+    @Id
     @JoinColumn(name = "employee_id")
     private Employee employee;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne // (fetch = FetchType.LAZY)
+    @Id
     @JoinColumn(name = "project_id")
     private Project project;
 
