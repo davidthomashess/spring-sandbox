43a520507fafe59c0e470c81bc30344dddcc61a2
ddtmhess <ddtmhess@gmail.com>
2024-07-02 17:35:03 +0000

    Removed unused import

--- src/test/java/com/sandbox/company/EmployeeServiceTest.java
+++ src/test/java/com/sandbox/company/EmployeeServiceTest.java
@@ -1,7 +1,6 @@
 package com.sandbox.company;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
